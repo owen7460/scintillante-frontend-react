b95939e2e5b48b55fbf596af68a1695313b2127d
Owen <Xiongwen.Ouyang@edu.sait.ca>
2026-05-01 23:47:19 -0600

    import tailwindCSS and adjust on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -23,22 +23,33 @@ function App() {
 
   return (
     <>
-      <h1>Scintillante</h1>
-      <p>
-        Scintillante is a relaxing and claming site to share French
-        Impressionist Music.
-      </p>
-      <div id="composers">
-        <h2>Composers</h2>
-        {composers.map((composer) => {
-          return (
-            <div key={composer.id}>
-              <h3 onClick={() => handleComposerClick(composer.id)}>
-                {composer.name}
-              </h3>
-            </div>
-          );
-        })}
+      <div className="flex flex-col items-center justify-center h-screen w-screen gap-4">
+        <h1 className="text-6xl font-thin">Scintillante</h1>
+        <p className="text-lg font-thin">
+          Scintillante is a relaxing and claming site to share French
+          Impressionist Music.
+        </p>
+        <div
+          id="composers"
+          className="flex flex-col items-center justify-center gap-4"
+        >
+          <h2 className="text-2xl font-thin">Composers</h2>
+          {composers.map((composer) => {
+            return (
+              <div
+                key={composer.id}
+                className="flex flex-col items-center justify-center gap-2"
+              >
+                <h3
+                  onClick={() => handleComposerClick(composer.id)}
+                  className="text-xl font-thin"
+                >
+                  {composer.name}
+                </h3>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );

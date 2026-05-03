9c09bfd0847e1c0ee515d4476c87c7ea34cdf8ff
Owen <Xiongwen.Ouyang@edu.sait.ca>
2026-05-03 00:46:30 -0600

    finish pagenation on Composers page

--- src/apis/getWorks.tsx
+++ src/apis/getWorks.tsx
@@ -1,7 +1,13 @@
 import http from "./http";
 
-export const getWorks = async (composerId: number) => {
-  const response = await http.get(`/composers/works?composerId=${composerId}`);
+export const getWorks = async (
+  composerId: number,
+  page: number = 1,
+  pageSize: number = 2,
+) => {
+  const response = await http.get(
+    `/composers/works?composerId=${composerId}&page=${page}&pageSize=${pageSize}`,
+  );
   return response.data.data;
 };
 
--- src/pages/Composer.tsx
+++ src/pages/Composer.tsx
@@ -1,33 +1,77 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import getWorks from "../apis/getWorks";
 import { useState, useEffect } from "react";
-function Composer() {
-  const { id } = useParams();
-  const [works, setWorks] = useState([]);
+import { useNavigate } from "react-router-dom";
 
+function Composer() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const page = searchParams.get("page") ? searchParams.get("page") : 1;
+  const pageSize = searchParams.get("pageSize")
+    ? searchParams.get("pageSize")
+    : "2";
+
+  const [works, setWorks] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const totalPages = Math.ceil(Number(total) / Number(pageSize));
   useEffect(() => {
     const fetchWorks = async () => {
       try {
-        const data = await getWorks(Number(id));
+        const data = await getWorks(Number(id), Number(page), Number(pageSize));
         setWorks(data.works);
-        console.log(data);
+        setTotal(data.total);
       } catch (error) {
         console.error("Error fetching works:", error);
       }
     };
     fetchWorks();
-  }, [id]);
+  }, [id, page, pageSize]);
+
+  const handleHomepageClick = () => {
+    navigate("/");
+  };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen gap-4">
-      <h1 className="text-6xl font-thin">Composer</h1>
-      <p className="text-lg font-thin">{id}</p>
+      <h1 className="text-6xl font-thin">Works</h1>
       {works.map((work) => (
         <div key={work.id}>
-          <p className="text-lg font-thin">{work.title}</p>
-          <p className="text-lg font-thin">{work.genre}</p>
+          <p className="text-lg font-thin">
+            {work.title} - {work.genre}
+          </p>
         </div>
       ))}
+      <div className="flex gap-2 mt-6">
+        {Array.from({ length: totalPages }).map((_, index) => {
+          const pageNumber = index + 1;
+
+          return (
+            <button
+              key={pageNumber}
+              onClick={() =>
+                setSearchParams({
+                  page: String(pageNumber),
+                  pageSize: String(pageSize),
+                })
+              }
+              className={`px-3 py-1 border cursor-pointer ${
+                Number(page) === pageNumber ? "bg-black text-white" : ""
+              }`}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button
+        onClick={() => handleHomepageClick()}
+        className="px-3 py-1 border cursor-pointer font-thin"
+      >
+        Homepage
+      </button>
     </div>
   );
 }
